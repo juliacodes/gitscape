9429374ca23510eb8b36497c39049c8596fd10cb
juliajcodes <john2122@purdue.edu>
2020-02-18 11:41:50 -0500

    Revert

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Container } from '../../Styling/styles';
-import { CardCont } from './styles';
+
 const Card = props => {
     return (
-        <Container style={{ margin: '1em' }}>
-            <CardCont>
-                <h1 key={props.id}>{props.repoName}</h1>
-                <p>{props.userName}</p>
-                <p>{props.description}</p>
-                <p>Forks: {props.forks}</p>
-                <p>stars: {props.stars}</p>
-                <p>issues: {props.issues}</p>
-            </CardCont>
-        </Container>
+        <div style={{ margin: '1em' }}>
+            <div>
+                <div style={{ fontWeight: 'bold' }}>{props.id}</div>
+                <div>{props.id}</div>
+            </div>
+        </div>
     );
 };
 
--- src/Components/Form/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const CardCont = styled.div`
-    width: 400px;
-    height: auto;
-    background-color: ${({ theme }) => theme.overlay};
-`;
